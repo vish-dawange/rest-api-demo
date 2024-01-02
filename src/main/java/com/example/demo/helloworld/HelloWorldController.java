74a19915374da0b608edd2ded043bfa67cbaabfa
Vishal Dawange <vishalsd27@gmail.com>
2024-01-01 18:05:20 -0600

    added support for internationalization

--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -1,6 +1,11 @@
 package com.example.demo.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // / hello-world
     @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
@@ -24,5 +35,12 @@ public class HelloWorldController {
     @RequestMapping(path = "/hello-world/path-variable/{name}", method = RequestMethod.GET)
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World," + name);
+    }
+
+    @RequestMapping(path = "/hello-world-i18n", method = RequestMethod.GET)
+    public String helloWorldI18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "default msg", locale);
+        // return "Hello World v2!!!";
     }
 }
